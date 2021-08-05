0e469a47b8ebca79aefc19edf917b80bf1d49655
ocean21726 <ocean21726@gmail.com>
2021-08-05 11:43:49 +0900

    문의게시판 detail

--- src/containers/board/QnaDetail.jsx
+++ src/containers/board/QnaDetail.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { boardService } from 'services';
+import QuillEditor from 'components/board/QuillEditor';
 import FormInput from 'components/board/Input';
 import Button from 'components/board/Button';
 
@@ -43,6 +44,20 @@ const QnaDetail = (props) => {
     console.log(res);
   };
 
+  const qnaModify = async () => {
+    console.log('qnaModify');
+  };
+
+  const qnaDelete = async () => {
+    const res = await boardService.delete('qna', props.idx);
+    loadData();
+    console.log(res);
+  };
+
+  const replyModify = async (condition) => {
+    console.log(condition);
+  };
+
   const replyDelete = async (condition) => {
     console.log(condition);
     const params = {
@@ -62,9 +77,14 @@ const QnaDetail = (props) => {
         <label>{detail.data.title}</label>
         <label>  조회 {detail.data.read_num} </label>
         <label>{detail.data.reg_user_id}</label>
-        <button id='qna_form_view'>수정</button>
-        <button id='delete_qna'>삭제</button><br /><br />
-        <label>{detail.data.content}</label><br />
+        <button onClick={() => qnaModify}>수정</button>
+        <button onClick={() => qnaDelete}>삭제</button><br /><br />
+        <QuillEditor
+          theme='bubble'
+          id='content'
+          value={detail.data.content ?? ''}
+          readOnly
+        />
       </div>
       <div>
         댓글
@@ -75,7 +95,8 @@ const QnaDetail = (props) => {
             return (
               <tr key={i}>
                 <label>{item.reg_user_id}  </label>
-                <label>{item.reg_date}</label>
+                <label>{item.reg_date}  </label>
+                <button onClick={() => replyModify(item)}>수정</button>
                 <button onClick={() => replyDelete(item)}>삭제</button><br />
                 <label>{item.qna_re_content}</label><br /><br />
               </tr>
